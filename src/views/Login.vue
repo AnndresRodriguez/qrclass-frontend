544271c6275ef6a7cdb3d0b30664172e2e5f1cec
angelica bermudez <angelicamariabs@ufps.edu.co>
2020-10-22 21:51:37 -0500

    [ADD] Login, vista

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <h2>Login</h2>
-    <router-link to="/dashboard" active-class="btn btn-primary">
-      Iniciar Sesión
-    </router-link>
+    <body >
+      <div class="wrapper fadeIn first">
+        <h2>ASISTENTE DE CLASE</h2><br/>
+        <img  id="lologin" src="img/ufps.png"/><br/><br/>
+        <h6>Identifiquese usando su cuenta institucional en:</h6><br/>
+        <button type="submit"  class="btn btn-light"><img src="https://accounts.google.com/favicon.ico" alt="" width="24" height="24"/>Login  with Google</button>
+    	</div>
+    </body>
   </div>
 </template>
+
+
+      
