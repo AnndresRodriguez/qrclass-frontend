2e98de1852efd97d38d44bcbaf4028f1597d51b6
angelica bermudez <angelicamariabs@ufps.edu.co>
2020-10-24 17:34:49 -0500

    [ADD] consumir api ejemplo

--- src/components/docente/horario.vue
+++ src/components/docente/horario.vue
@@ -3,3 +3,31 @@
     <h2>Horario Docente</h2>
   </div>
 </template>
+
+
+<script>
+import axios from "axios";
+export default {
+mounted(){
+  this.abriApi();
+},
+
+methods: {
+
+   abriApi() {
+    axios.get("https://removal-api.herokuapp.com/service")
+    .then(respuesta => {console.log(respuesta.data)})
+  },
+
+
+
+
+
+}
+
+  
+}
+</script>
+<style scoped>
+
+</style>
