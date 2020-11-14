502992f1e755270268cbfea521f8c154e4cb91f8
Andres Carrillo <andresjoe24@gmail.com>
2020-11-14 11:22:07 -0500

    [ADD] Registro nuevo docente

--- src/components/modulo-admin/docente/docente-registro.vue
+++ src/components/modulo-admin/docente/docente-registro.vue
@@ -3,18 +3,19 @@
     <div class="col-md-10 cssRegistro">
       <h2>Registrar Docente</h2>
       <hr />
-      <form>
+      <form @submit.prevent="registrarDocente">
         <div class="form-group">
           <label class="control-label">Nombre Completo</label>
           <div class="input-group">
             <div class="input-group-addon">
-              <i class=" icono fas fa-user"></i>
+              <i class="icono fas fa-user"></i>
             </div>
             <input
               name="name"
               type="text"
               class="form-control"
               placeholder="Escriba el nombre completo del docente a registrar"
+              v-model="nombre"
               required
             />
           </div>
@@ -31,6 +32,7 @@
               class="form-control"
               placeholder="Escriba el numero de codigo del docente"
               required
+              v-model="codigo"
             />
           </div>
         </div>
@@ -40,11 +42,18 @@
             <div class="input-group-addon">
               <i class="icono fas fa-check-square"></i>
             </div>
-            <select name="departamento" class="form-control">
-              <option value="dptoMa">Departamento de Matematicas</option>
-              <option value="dptoFi">Departamento de Fisica</option>
-              <option value="dptoEl">Departamento de Electronica</option>
-              <option value="dptoDi">Departamento de Diseño</option>
+            <select
+              @change="listarDepartamentos(departamento)"
+              v-model="departamento"
+              name="departamento"
+              class="form-control"
+            >
+              <template v-for="(departamento, index) in departamentos">
+                <option :key="index" :value="departamento">
+                  {{ departamento.idDepartamento }} -
+                  {{ departamento.nombre }}
+                </option>
+              </template>
             </select>
           </div>
         </div>
@@ -53,13 +62,14 @@
           <label class="control-label">Correo Electronico</label>
           <div class="input-group">
             <div class="input-group-addon">
-              <i class=" icono fas fa-at"></i>
+              <i class="icono fas fa-at"></i>
             </div>
             <input
               type="email"
               class="form-control"
               placeholder="Escriba el corre institucional (example@ufps.edu.co)"
               required
+              v-model="correo"
             />
           </div>
         </div>
@@ -74,16 +84,73 @@
               class="form-control"
               placeholder="Escriba el numero de celular."
               required
+              v-model="telefono"
             />
           </div>
         </div>
         <div class="boton form-group">
-          <button type="submit" class=" btn btn-info">Registrarse</button>
+          <button type="submit" class="btn btn-info">Registrarse</button>
         </div>
       </form>
     </div>
   </div>
 </template>
+
+<script>
+/* eslint-disable */
+export default {
+  data() {
+    return {
+      docentes: [],
+      nombre: "",
+      codigo: "",
+      correo: "",
+      telefono: "",
+      departamento: {},
+      departamentos: [],
+      iddepartamento: 0,
+    };
+  },
+  created() {
+    this.getAllDepartamentos();
+  },
+  methods: {
+    getAllDepartamentos() {
+      axios
+        .get(`${process.env.VUE_APP_API}/departamentos`)
+        .then((res) => {
+          console.log(res.data.operation);
+          this.departamentos = res.data.data;
+          console.log("departamentos -->", this.departamentos);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    listarDepartamentos(departamento) {
+      this.iddepartamento = departamento.idDepartamento;
+    },
+
+    registrarDocente(){
+
+      const docente = { idDocenteCodigo: this.codigo, nombre: this.nombre, correo: this.correo, telefono: this.telefono, idDepartamento: this.iddepartamento }
+
+      // console.log('docenteCreado', docente)
+
+      axios.post(`${process.env.VUE_APP_API}/docentes`, docente)
+      .then( res => {
+         console.log(res.data);
+      })
+      .catch(error => {
+        console.log('registrarDocente', error)
+      })
+
+    }
+
+  },
+};
+</script>
 
 <style scoped>
 .cssRegistro .form-group {
