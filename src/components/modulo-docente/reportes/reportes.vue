829781fd1eeb1565ce7adade52df3488ce7259d1
angelica bermudez <angelicamariabs@ufps.edu.co>
2020-11-30 16:13:46 -0500

    [ADD] reportes docente enlaces

--- src/components/modulo-docente/reportes/reportes.vue
+++ src/components/modulo-docente/reportes/reportes.vue
@@ -3,6 +3,28 @@
     <div class="col-md-10 cssRegistro">
       <h2>Generar Reportes</h2>
       <hr />
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Codigo: </label>
+        <div class="col-sm-10">
+          <form class="navbar-form navbar-left" action="/action_page.php">
+            <div class="input-group">
+              <input
+                v-model="ccodigo"
+                type="email"
+                class="form-control"
+                placeholder="Escriba el codigo de la materia consultar"
+              />
+              <div class="input-group-btn">
+                <button class="btn btn-info" type="submit">
+                  <i class="iconob fas fa-search"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <br />
+      <br />
       <h5 class="h5L">Lista de Materias</h5>
       <hr />
       <table class="table table-hover">
@@ -10,19 +32,22 @@
           <tr>
             <th scope="col">Codigo</th>
             <th scope="col">Nombre</th>
-            <th scope="col">Reporte Materia</th>
+            <th scope="col">Reporte</th>
             <th scope="col">Lista Estudiantes</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1151103</th>
-            <td>Programacion I</td>
+          <tr v-for="(materia, index) in filtrarMateria" :key="index">
+            <th scope="row">{{ materia.codigo }}</th>
+            <td>{{ materia.nombre }}</td>
             <td>Generar</td>
             <td>
-              <router-link :to="{ name: 'reporte-estudiante' }"
-                >Ver</router-link
+              <button
+                class="btn btn-light"
+                @click="verEstudiantesMateria(materia)"
               >
+                Ver
+              </button>
             </td>
           </tr>
         </tbody>
@@ -30,5 +55,73 @@
     </div>
   </div>
 </template>
-<script></script>
-<style scoped></style>
+
+<script>
+/* eslint-disable */
+export default {
+  data() {
+    return {
+      materiasDocente: [],
+      materia: {},
+      codigo: "",
+      id: 0,
+      nombre: "",
+      ccodigo: "",
+    };
+  },
+
+  created() {
+    this.getAllDataDocente();
+    // axios.post()
+  },
+
+  methods: {
+    getAllDataDocente() {
+      axios
+        .post(
+          `${process.env.VUE_APP_API}/materias/${this.$store.getters.getInfoRole.id}`
+        )
+        .then((res) => {
+          this.materiasDocente = res.data.data;
+          console.log("materias por docente", this.materiasDocente);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    verEstudiantesMateria(materia) {
+      this.$store.dispatch("listarEstudianteMateria", materia);
+      this.$router.replace({ name: "reporte-estudiante" });
+      console.log("materia que envio de materia asistencia", materia);
+    },
+  },
+  computed: {
+    filtrarMateria() {
+      return this.materiasDocente.filter((materia) =>
+        materia.codigo.toLowerCase().includes(this.ccodigo)
+      );
+    },
+  },
+};
+</script>
+
+<style scoped>
+.bordeInput {
+  background: white;
+  border: white;
+  width: 60%;
+}
+.cssRegistro .modal-title {
+  text-align: center;
+  color: rgb(188, 0, 22);
+}
+.cssRegistro .form-group {
+  margin-bottom: 0rem;
+}
+.table-hover tbody tr:hover td,
+.table-hover tbody tr:hover th,
+.icono:hover {
+  background-color: rgb(188, 0, 22);
+  color: #ffffff;
+}
+</style>
