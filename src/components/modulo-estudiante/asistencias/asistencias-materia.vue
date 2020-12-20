b14718f391829eaa6039b27dbdb6e9d1181fa254
angelica bermudez <angelicamariabs@ufps.edu.co>
2020-12-19 23:39:15 -0500

    [ADD] modulo Estudiante completo

--- src/components/modulo-estudiante/asistencias/asistencias-materia.vue
+++ src/components/modulo-estudiante/asistencias/asistencias-materia.vue
@@ -1,52 +1,122 @@
 <template>
   <div class="d-flex justify-content-center">
     <div class="col-md-10 cssRegistro">
-      <h2>Lista de Asistencia</h2>
+      <h2>Lista de Materias</h2>
       <hr />
+      <br />
       <div class="scroll-div">
         <table class="table table-hover">
           <thead>
             <tr>
               <th scope="col">Codigo</th>
               <th scope="col">Materia</th>
-              <th scope="col">Fecha</th>
-              <th scope="col">Asistio</th>
+              <th scope="col">Asistencias</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(estudiante, index) in estudiantes" :key="index">
-              <th scope="row">{{ estudiante.codigo }}</th>
+            <tr v-for="(materia, index) in materias" :key="index">
+              <th scope="row">{{ materia.codigo }}</th>
               <td>{{ materia.nombre }}</td>
-              <template v-for="(asistencia, indice) in estudiante.asistencias">
-                <th scope="row" :key="indice">
-                  <template v-if="asistencia.asistio === 1">
-                    <i class="fas fa-check fa-2x" style="color:#69E243"></i>
-                  </template>
-                  <template v-else>
-                    <i class="fas fa-minus fa-2x" style="color:red"></i>
-                  </template>
-                </th>
-              </template>
-              <th>{{ calculateAsistencias(estudiante.asistencias) }}</th>
+              <td>
+                <a
+                  data-toggle="modal"
+                  data-target="#ver"
+                  class="btn btn-light"
+                  @click="verAsistencias(materia)"
+                  >Ver
+                </a>
+              </td>
             </tr>
           </tbody>
         </table>
+      </div>
+      <div class="cssRegistro">
+        <div
+          class="modal fade"
+          id="ver"
+          tabindex="-1"
+          role="dialog"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title">Asistencias</h4>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div>
+                  <label class="col-form-label">Materia: </label>
+                  <input class="bordeInput" disabled v-model="nombre" /><br />
+                  <label class="col-form-label">Codigo: </label>
+                  <input class="bordeInput" disabled v-model="codigo" /><br />
+                </div>
+
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th scope="col">Id</th>
+                      <th scope="col">Fecha</th>
+                      <th scope="col">Asistio</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      v-for="(asistencia, index) in materias[0].asistencias"
+                      :key="index"
+                    >
+                      <th>{{ index + 1 }}</th>
+                      <th scope="row">
+                        {{ formatAssistanceEstudiante(asistencia.createdAt) }}
+                      </th>
+                      <td>
+                        <template v-if="asistencia.asistio === 1">
+                          <i
+                            class="fas fa-check fa-2x"
+                            style="color: #69e243"
+                          ></i>
+                        </template>
+                        <template v-else>
+                          <i class="fas fa-minus fa-2x" style="color: red"></i>
+                        </template>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="modal-footer">
+                <a data-dismiss="modal" aria-label="Close" class="btn btn-info"
+                  >Volver</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 /* eslint-disable */
-import { formatAssistance } from "../../../util/tools";
+import { formatDate } from "../../../util/tools";
 export default {
   data() {
     return {
-     estudiantes: [],
-     estudiante: {},
-     materias: [],
-     materia:{},
-     asistencias:[],
-     asistencia: {},
+      estudiantes: [],
+      estudiante: {},
+      materias: [],
+      materia: {},
+      asistencia: {},
+      id: 0,
+      nombre: "",
+      codigo: "",
     };
   },
 
@@ -63,34 +133,35 @@ export default {
         )
         .then((res) => {
           this.estudiantes = res.data.data;
+          console.log("estudiantes", this.estudiantes);
 
-          res.data.data.map(estudiante => {
-                this.materias = this.estudiante.materias;
-            });
-          console.log(this.estudiantes);
+          res.data.data.map((estudiante) => {
+            this.materias = estudiante.materias;
+          });
+          console.log("materiaaa", this.materias);
         })
         .catch((err) => {
           console.log(err);
         });
     },
-    getAllDataMaterias(){
-
+    verAsistencias(materia) {
+      this.id = materia.id;
+      this.codigo = materia.codigo;
+      this.nombre = materia.nombre;
     },
-     calculateAsistencias(asistencias){
 
-      console.log('calculateAsistencias', asistencias);
+    calculateAsistencias(asistencias) {
+      console.log("calculateAsistencias", asistencias);
       let asistenciasTotales = 0;
 
       asistenciasTotales = asistencias.reduce(function (acc, asistencia) {
-          return acc + asistencia.asistio;
-      },0);
+        return acc + asistencia.asistio;
+      }, 0);
 
-      return `${asistenciasTotales}/${asistencias.length}`
-
+      return `${asistenciasTotales}/${asistencias.length}`;
     },
-
-    formatAssistance(formatDate) {
-      return formatAssistance(new Date(formatDate));
+    formatAssistanceEstudiante(formatDateE) {
+      return formatDate(new Date(formatDateE));
     },
   },
 };
--- src/components/modulo-estudiante/datos/datos-estudiante.vue
+++ src/components/modulo-estudiante/datos/datos-estudiante.vue
@@ -4,19 +4,152 @@
       <h2>Mis Datos</h2>
       <hr />
       <br />
+      <form @submit.prevent="actualizarEstudiante">
+        <div class="form-group">
+          <label class="control-label labD">Nombre Completo</label>
+          <input
+            name="name"
+            type="text"
+            class="form-control bordeInput"
+            disabled
+            v-model="nombre"
+          />
+        </div>
+        <div class="form-group">
+          <label class="control-label">Codigo:</label>
+          <input
+            name="name"
+            type="text"
+            class="form-control bordeInput"
+            disabled
+            v-model="codigo"
+          />
+        </div>
+        <div class="form-group">
+          <label class="control-label">Correo Electronico</label>
+          <input
+            type="email"
+            class="form-control bordeInput"
+            disabled
+            v-model="correo"
+          />
+        </div>
+        <div class="form-group">
+          <label class="control-label">Numero de Telefono</label>
+          <input
+            type="text"
+            class="form-control"
+            @keypress="validateNumber"
+            placeholder="Escriba el numero de celular."
+            maxlength="10"
+            minlength="7"
+            required
+            v-model="telefono"
+          />
+        </div>
+        <div class="boton form-group">
+          <button type="submit" class="btn btn-info">Actualizar</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
+
+import { fireToast } from "../../../util/toast";
+import { onlyNumbers } from "../../../util/tools";
+import $ from "jquery";
+export default {
+  data() {
+    return {
+      id: 0,
+      nombre: "",
+      codigo: "",
+      correo: "",
+      telefono: "",
+      estadoE: 1,
+      ccorreo: "",
+    };
+  },
+  created() {
+    this.getEstudiante();
+  },
+  methods: {
+    getEstudiante() {
+      axios
+        .get(
+          `${process.env.VUE_APP_API}/estudiantes/${this.$store.getters.getInfoRole.id}`
+        )
+        .then((res) => {
+          console.log(res.data.operation);
+          this.id = res.data.data.id;
+          this.nombre = res.data.data.nombre;
+          this.codigo = res.data.data.codigo;
+          this.correo = res.data.data.correo;
+          this.telefono = res.data.data.telefono;
+          this.estadoE = res.data.data.estado;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    actualizarEstudiante() {
+      const estudiante = {
+        nombre: this.nombre,
+        documento: this.documento,
+        correo: this.correo,
+        telefono: this.telefono,
+        estado: this.estadoE,
+      };
+      axios
+        .put(`${process.env.VUE_APP_API}/estudiantes`, {
+          idEstudiante: this.id,
+          estudiante,
+        })
+
+        .then((res) => {
+          if (res.data.operation) {
+            console.log(res.data);
+
+            fireToast(
+              "success",
+              "Actualización Exitosa",
+              "Los datos del estudiante han sido actualizados"
+            );
+            this.getEstudiante();
+          } else {
+            console.log(res.data);
+
+            fireToast(
+              "error",
+              "Error en la actualización",
+              "Ha ocurrido un error al actualizar los datos del estudiante, intente nuevamente"
+            );
+          }
+        });
+      console.log(estudiante);
+      this.getEstudiante();
+    },
+    validateNumber($event) {
+      onlyNumbers($event);
+    },
+  },
+  computed: {
+    filtrarEstudiante() {
+      return this.estudiantes.filter((estudiante) =>
+        estudiante.correo.toLowerCase().includes(this.ccorreo)
+      );
+    },
+  },
+};
 </script>
 
 <style scoped>
 .bordeInput {
-  background: white;
-  border: white;
-  width: 60%;
+  background: white !important;
+  border: white !important;
 }
 .cssRegistro .modal-title {
   text-align: center;
